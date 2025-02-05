adadbdbd8ecd15aa04e3c8b183596ea03cf3597e
AamirSohail07000 <sohail.aamir07000@gmail.com>
2025-02-06 00:54:47 +0530

    lambda function to find the sum of squares of all numbers in a given list using map() and sum()

---
+++ coding_practice/Python/lambda4.py
@@ -0,0 +1,11 @@
+# Write a lambda function to find the sum of squares of all numbers in a given list using map() and sum().
+
+list1 = [2,1,3,4]
+sumof_square= sum(map(lambda x: x**2,list1))
+print(sumof_square)
+
+
+#Use of sum
+# print(sum(list1))
+# num= {2,3,4,5}
+# print(sum(num))
