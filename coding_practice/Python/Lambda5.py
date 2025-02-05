7c80ecb4347053cb2f83ae2c09d1d573336f7ad3
AamirSohail07000 <sohail.aamir07000@gmail.com>
2025-02-06 05:21:58 +0530

    ambda function inside sorted() to sort a list of tuples based on the second value

--- coding_practice/Python/Lambda5.py
+++ coding_practice/Python/Lambda5.py
@@ -1,5 +1,5 @@
 # Use a lambda function inside sorted() to sort a list of tuples based on the second value in each tuple.
-# Write a lambda function to generate a Fibonacci sequence up to a given number using reduce().
+
 
 # List of tuples
 tuples_List =[(1,2),(3,4),(1,4),(1,1)]
