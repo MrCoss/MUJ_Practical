fbca53e5e2f560e89c37777277b4cc07bdf92058
AamirSohail07000 <sohail.aamir07000@gmail.com>
2025-03-12 23:03:48 +0530

    pan card problem statement

---
+++ Python/practical/pancard_regex.py
@@ -0,0 +1,8 @@
+# Write a python program to validate input PAN number using a regular expression 
+# PAN Format:
+# PAN is a 10-character alphanumeric identifier.
+# The format is: AAAAA9999A, where:
+# First 5 characters → Uppercase letters (A-Z).
+# Next 4 characters → Digits (0-9).
+# Last 1 character → Uppercase letter (A-Z).
+
