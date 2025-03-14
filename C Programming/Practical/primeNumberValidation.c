3ba0f05be47187f9555c379824633083a9ba0bf6
AamirSohail07000 <sohail.aamir07000@gmail.com>
2025-03-14 15:51:18 +0530

    Checking prime number

--- C Programming/Practical/primeNumberValidation.c
+++ C Programming/Practical/primeNumberValidation.c
@@ -1,11 +1,12 @@
 // Check whether a given number is prime or not
 
 #include <stdio.h>
-
+// Function to check prime
 int isPrime(int num){
-  if (num < 2)
+  if (num < 2) // Numbers less than 2 are not prime
+
     return 0;
-  for (int i =2; i * i <=num; i++){
+  for (int i =2; i * i <=num; i++){ // Loop up to sqrt(num)
     if (num % i == 0)
       return 0; // number is divisible hence not a prime
   }
@@ -14,10 +15,11 @@ int isPrime(int num){
 
 int main(){
   int num;
+  // Taking input from the user
   printf("Enter a number:");
   scanf("%d", &num);
   
-  //Check if given number is prime
+  //Checking if given number is prime
   if (isPrime(num))
     printf("%d is a prime number\n", num);
   else
