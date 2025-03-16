17f12da589300c9a402022cc509799cec5b5bcd7
AamirSohail07000 <sohail.aamir07000@gmail.com>
2025-03-16 17:54:51 +0530

    reverse a string using loop and function

--- C Programming/Practical/ReverseAString.c
+++ C Programming/Practical/ReverseAString.c
@@ -2,22 +2,50 @@
 #include <stdio.h>
 #include <string.h>
 
-int main() {
-    char str[100], temp;
-    int i, len;
+// int main() {
+//     char str[100], temp;
+//     int i, len;
 
-    // Taking input
-    printf("Enter a string: ");
-    fgets(str, sizeof(str), stdin);
+//     // Taking input
+//     printf("Enter a string: ");
+//     fgets(str, sizeof(str), stdin);
 
-    len = strlen(str);  // Finding length of the string
+//     len = strlen(str);  // Finding length of the string
 
-    // Swapping characters from both ends
+//     // Swapping characters from both ends
+//     for (i = 0; i < len / 2; i++) {
+//         temp = str[i];
+//         str[i] = str[len - i - 1];
+//         str[len - i - 1] = temp;
+//     }
+
+//     printf("Reversed string: %s\n", str);
+//     return 0;
+// }
+
+// Using function
+
+
+
+// Function to reverse a string
+void reverseString(char str[]) {
+    int i, len = strlen(str);
+    char temp;
+
     for (i = 0; i < len / 2; i++) {
         temp = str[i];
         str[i] = str[len - i - 1];
         str[len - i - 1] = temp;
     }
+}
+
+int main() {
+    char str[100];
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    reverseString(str);  // Function call
 
     printf("Reversed string: %s\n", str);
     return 0;
