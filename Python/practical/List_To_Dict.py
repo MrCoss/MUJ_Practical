81e873fdef59f2a60be0aa2cbbb998d23953fb5c
AamirSohail07000 <sohail.aamir07000@gmail.com>
2025-03-17 23:47:18 +0530

    updated explaination for dict comprehension

--- Python/practical/List_To_Dict.py
+++ Python/practical/List_To_Dict.py
@@ -12,5 +12,7 @@ print("Original Dictionary:", data_dict)
 inverted_dict = {value: key for key, value in data_dict.items()}
 print("Inverted Dictionary:", inverted_dict)
 
+# Explaination : value: key} → This part is deciding the arrangement (Value first, Key second).
+# for key, value in data.items() → This part is fetching data from data.items() and providing the key-value pairs to swap.
 # Note:- Swap keys and values using dictionary comprehension:
 # {value: key for key, value in data_dict.items()}
