037efcb3fe3e99c6f606eab78d03fc7d75c927aa
AamirSohail07000 <sohail.aamir07000@gmail.com>
2025-03-22 23:44:45 +0530

    python script using numpy to create a 3 x 3 matrix with values ranging from 1 to 9

---
+++ Python/practical/Numpy_3x3_matrix.py
@@ -0,0 +1,10 @@
+# Write a python script using numpy to create a 3 x 3 matrix with values ranging from 1 to 9
+
+import numpy as np
+
+# Create a 3x3 matrix with values from 1 to 9
+matrix = np.arange(1, 10).reshape(3, 3)
+
+# Print the matrix
+print(matrix)
+
