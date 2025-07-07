e92c406a8a156ac2f6905dd6929904e8645fa589
AamirSohail07000 <sohail.aamir07000@gmail.com>
2025-07-08 01:42:08 +0530

    adding two numbers

---
+++ Java/AddingTwoNumbers.java
@@ -0,0 +1,11 @@
+// Program to add two numbers
+import java.util.Scanner;
+public class AddingTwoNumbers{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+
+    System.out.println(a+b);
+  }
+}
