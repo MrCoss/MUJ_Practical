6ee632201e678f8c7fe6d6b75db18f50a78e4c5b
AamirSohail07000 <sohail.aamir07000@gmail.com>
2025-07-11 02:47:33 +0530

    Sum of first N natural numbers using for loop

---
+++ Java/practice/Loops and numbers/SumOfNaturalNumbers.java
@@ -0,0 +1,20 @@
+// Sum of first N natural numbers
+
+import java.util.Scanner;
+
+public class SumOfNaturalNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter value of N: ");
+        int n = sc.nextInt();
+
+        int sum = 0;
+        for (int i = 1; i <= n; i++)
+            sum += i;
+
+        System.out.println("Sum of first " + n + " natural numbers is: " + sum);
+        
+        sc.close();
+    }
+}
